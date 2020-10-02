672e79f262d26b50bf03e5ff5b325574deaca1eb
Ishan Goyal <ishan0359.cse19@chitkara.edu.in>
2020-10-02 10:09:33 +0530

    Main1

---
+++ main1.js
@@ -0,0 +1,17 @@
+/*function largeImage(x){
+    x.style.width="425px";
+    x.style.height="325px";
+}*/
+/*function normalImage(x){
+    x.style.width="400px";
+    x.style.height="300px";
+}*/
+
+function largebutton(x){
+    x.style.width="180px";
+    x.style.height="50px";
+}
+function normalbutton(x){
+    x.style.width="150px";
+    x.style.height="40px";
+}
